4eec81ac1fc7fe61ef17507a097f95680aecdf80
facuuu11 <seconecto.elchipa@gmail.com>
2024-09-20 12:43:31 -0300

    menu interfaz

--- interfaz/src/interfaz/Main.java
+++ interfaz/src/interfaz/Main.java
@@ -8,28 +8,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		LinkedList<Usuario> usuarios = new LinkedList<Usuario>();
 		
-		usuarios.add(new Usuario("Pitaev","1234","admin"));
-		String mail = JOptionPane.showInputDialog("Ingrese mail");
-		String cont = JOptionPane.showInputDialog("Ingrese contraseña");
-		
-		Usuario encontrado = Usuario.Login(usuarios, mail, cont) ;
-		
-		if(encontrado!=null) {
-			JOptionPane.showMessageDialog(null, encontrado);
-			if (encontrado.getRol().equals("admin")) {
-				
-				JOptionPane.showMessageDialog(null, "olis");
-//				Admin nuevo = new Admin(encontrado.getMail(""),encontrado.getContrasena(""),encontrado.getRol(""));
-//				nuevo.menuPrincipal();
-			} else if (encontrado.getRol().equals("encar")) {
-			
-		}else {
-			JOptionPane.showMessageDialog(null, "No existe");
 
-		}
+
 		
-	}
+		
+		
+		
+		
+//		LinkedList<Usuario> usuarios = new LinkedList<Usuario>();
+//		
+//		usuarios.add(new Usuario("Pitaev","1234","admin"));
+//		String mail = JOptionPane.showInputDialog("Ingrese mail");
+//		String cont = JOptionPane.showInputDialog("Ingrese contraseña");
+//		
+//		Usuario encontrado = Usuario.Login(usuarios, mail, cont) ;
+//		
+//		if(encontrado!=null) {
+//			JOptionPane.showMessageDialog(null, encontrado);
+//			if (encontrado.getRol().equals("admin")) {
+//				
+//				JOptionPane.showMessageDialog(null, "olis");
+////				Admin nuevo = new Admin(encontrado.getMail(""),encontrado.getContrasena(""),encontrado.getRol(""));
+////				nuevo.menuPrincipal();
+//			} else if (encontrado.getRol().equals("encar")) {
+//			
+//		}else {
+//			JOptionPane.showMessageDialog(null, "No existe");
+//
+//		}
+		
+//	}
 }
 }
