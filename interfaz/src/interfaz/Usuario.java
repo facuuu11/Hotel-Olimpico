5a944d9a6cbf0ce9c56b1f8af279ed0a175333b3
facuuu11 <seconecto.elchipa@gmail.com>
2024-09-24 10:31:05 -0300

    Interfaz (Demo)

--- interfaz/src/interfaz/Usuario.java
+++ interfaz/src/interfaz/Usuario.java
@@ -5,48 +5,33 @@ import java.util.LinkedList;
 public class Usuario {
 	private String mail;
 	private String contrasena;
-	private String rol;
 	
-	public Usuario(String mail, String contrasena, String rol) {
+	
+	public Usuario(String mail, String contrasena) {
 		super();
 		this.mail = mail;
 		this.contrasena = contrasena;
-		this.rol = rol;
 	}
-
 	public String getMail() {
 		return mail;
 	}
-
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
-
 	public String getContrasena() {
 		return contrasena;
 	}
-
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
-
-	public String getRol() {
-		return rol;
-	}
-
-	public void setRol(String rol) {
-		this.rol = rol;
-	}
-
 	@Override
 	public String toString() {
-		return "Usuario [mail=" + mail + ", contrasena=" + contrasena + ", rol=" + rol + "]";
+		return "Usuario [mail=" + mail + ", contrasena=" + contrasena + "]";
 	}
-	
-	public static Usuario Login(LinkedList<Usuario> usuarios, String mail, String cotrasena ) {
+	public static Usuario Login(LinkedList<Usuario> usuarios, String mail, String contrasena ) {
 	
 		for (Usuario usuario : usuarios) {
-			if (usuario.getMail().equals(mail) && usuario.getContrasena().equals(cotrasena)) {
+			if (usuario.getMail().equals(mail) && usuario.getContrasena().equals(contrasena)) {
 				return usuario;
 			}
 		}
